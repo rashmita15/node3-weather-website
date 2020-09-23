d3550d79de6c562454ac083997f2dcede9423de0
rashmita15 <rashmita.pinto@gmail.com>
2020-09-23 20:33:14 +0530

    fix bug

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = proccess.env.Port || 3000
+const port = process.env.Port || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
